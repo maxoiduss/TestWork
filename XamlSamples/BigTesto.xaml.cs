4a2457b348e2e8359d403e3e6b6f7538592af876
Maksim Bass <m.bass@unlitsolutions.com>
2017-04-04 21:06:48 +0300

    Uahahaha!

---
+++ XamlSamples/BigTesto.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamlSamples
+{
+	public partial class BigTesto : ContentPage
+	{
+		public BigTesto()
+		{
+			InitializeComponent();
+		}
+	}
+}
